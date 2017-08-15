27b1867c4e3335b95685ea7793af33848ad5c4b1
JD <jdbrossillon@fly-n-sense.com>
2017-08-15 22:43:12 +0200

    clever config keys replaced by cold-hearted hash in filename

--- config.py
+++ config.py
@@ -2,22 +2,27 @@
 import json
 import os.path
 import sys
+import hashlib
 
 class SoundBoardConfig:
   
   def __init__(self):
     self.kvs = {}
     self.pgname = sys.argv[0]
-    print "config pgname",self.pgname
-    self.filename = os.path.join(os.path.expanduser('~'),'.soundboard.json')
+    
+    # whatever works (c)
+    h = hashlib.sha256()
+    h.update(self.pgname)
+    self.filename = os.path.join(os.path.expanduser('~'),
+                                  '.soundboard.%s.json'%(h.hexdigest()))
     self.load()
 
   def key(self, xy, k):
     if xy is None:
-      return "%s:global:%s"%(self.pgname,k)
+      return "global:%s"%(k)
     else:
       x,y = xy
-      return "%s:%d:%d:%s"%(self.pgname,x,y,k)
+      return "%d:%d:%s"%(x,y,k)
 
   def set(self, xy, k, v):
     print "set",xy,k,v
